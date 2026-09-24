2ba1e5d78eb3e3cda59592ace822053d47388c1e
paulino-david <paulinodvidn@gmail.com>
2026-09-24 21:48:56 +0200

    Explicaciones de cada linea de codigo

--- cliente.py
+++ cliente.py
@@ -1,14 +1,18 @@
 import socket
+import os
+from dotenv import load_dotenv
+load_dotenv()
 
-# sistema,nada,[IP_Ethernet,IP_Wifi]=socket.gethostbyname_ex(socket.gethostname())
-IP_Servidor="192.168.1.189"
-puerto=8000
+sistema,nada,[IP_Ethernet,IP_Wifi]=socket.gethostbyname_ex(socket.gethostname())
+
+IP_Servidor=os.getenv("IP_Wifi")
+puerto=os.getenv("Port")
 
 #Crear el socket / la conexion del servidor
 server=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-server.connect((IP_Servidor,puerto))
-
-server.send("Hola mundo".encode("utf-8"))
+server.connect((IP_Servidor,int(puerto)))
+ 
+server.send("Hola mundo soy {}".format("el sistema "+sistema if sistema!=socket.gethostname() else "mi propio sistema y me estoy cominicando conmigo mismo").encode("utf-8"))
 print(server.recv(1024).decode('utf-8'))
 
 
--- servidor.py
+++ servidor.py
@@ -1,27 +1,42 @@
 import socket
+from dotenv import load_dotenv,set_key
+
+load_dotenv()
 
 #La definicion de como y para que sera el sevidor
 #AF_INET: Para decir que es para internet
 #SOCK_STREAM: Para decir que la comunicacion sera de protocolo TCP de transporte
-server=socket.socket(socket.AF_INET,socket.SOCK_STREAM) #Para crear un nuevo socket; para crear conecciones
+server=socket.socket(socket.AF_INET,socket.SOCK_STREAM) #Para crear un nuevo socket; en este caso es un socket de tipo TCP/IP para internet
 
-# Para obtener los datos de red del dispositivo; nombre, IP de cable ethernet y la IP Wifi que viene dentro de una tupla
+# Para obtener los datos de red del sistema; nombre, IP de cable ethernet y la IP Wifi que viene dentro de una tupla
 nombreSistema,nada,[IP_Ethernet,IP_Wifi]=socket.gethostbyname_ex(socket.gethostname())
 #Para definir el puerto del socket
 port=8000
 
+#Para definir/asignar la IP del servidor y el puerto del socket; 
 server.bind((IP_Wifi,port))
+
+#Para guardar la IP y el puerto en el archivo .env
+set_key(".env","IP_Wifi",IP_Wifi)
+set_key(".env","Port",str(port))
 
 # Para indicar cuantas conexiones permite (limite maximo)
 server.listen(5)
 
 #Es para activar la conexion del servidor definitivamente hasta que sea false
 while True:
+    #Para aceptar la conexion del cliente y guardar el socket de comunicacion y la IP del cliente
     comunication_socket,IP_cliente=server.accept()
     print("Conected to {}".format(IP_cliente))
+
+# Para recibir el mensaje del cliente y decodificarlo a UTF-8 porque el servidor no actua como cliente y no puede recibir bytes, sino que recibe un mensaje en texto plano
+# Y porque wl servidor no puede actuar sin que el cliente le envie un mensaje, por eso se hace un recv() para recibir el mensaje del cliente
     mensaje_from_client=comunication_socket.recv(1024).decode("utf-8")
     print("Message from client {}".format(mensaje_from_client))
+
+#Para responderle al cliente con un mensaje de texto plano codificado a UTF-8 porque el cliente no actua como servidor y no puede recibir un mensaje en texto plano, sino que recibe bytes
     comunication_socket.send("Got your connection".encode("utf-8"))
+#Para cerrar la conexion del socket de comunicacion con el cliente y que el servidor pueda seguir escuchando a otros clientes
     comunication_socket.close()
     print("Connection with {} ended!".format(IP_cliente))
 
